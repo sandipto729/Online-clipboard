37759e969e78f393ad755f6413340e3144d23367
Arnab-pachal <ap.23cs8031@nitdgp.ac.in>
2025-07-28 21:03:14 +0530

    azure_blob.js file created

--- client/src/helper/azure_blob.js
+++ client/src/helper/azure_blob.js
@@ -1,20 +1,23 @@
-// const { BlobServiceClient } = require('@azure/storage-blob');
+
 import { BlobServiceClient } from '@azure/storage-blob';
 import path from 'path';
 
-const AZURE_STORAGE_CONNECTION_STRING = import.meta.env.VITE_AZURE_STORAGE_CONNECTION_STRING;
+// const AZURE_STORAGE_CONNECTION_STRING = import.meta.env.VITE_AZURE_STORAGE_CONNECTION_STRING;
+const AZURE_STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=your_account;AccountKey=your_key;EndpointSuffix=core.windows.net"
+if (!AZURE_STORAGE_CONNECTION_STRING) {
+  throw new Error('AZURE_STORAGE_CONNECTION_STRING is not set!');
+}
 
 async function uploadFile(containerName='checkfile', localFilePath) {
   const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
-
   const containerClient = blobServiceClient.getContainerClient(containerName);
+  await containerClient.createIfNotExists();
+
   const blobName = path.basename(localFilePath);
   const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
-  const uploadBlobResponse = await blockBlobClient.uploadFile(localFilePath);
-//   console.log(✅ File uploaded to: ${blockBlobClient.url});
-    console.log(`✅ File uploaded to: ${blockBlobClient.url}`);
-    return blockBlobClient.url; 
+  await blockBlobClient.uploadFile(localFilePath);
+  console.log(`✅ File uploaded to: ${blockBlobClient.url}`);
 }
 
-export default uploadFile; 
+export default uploadFile;
